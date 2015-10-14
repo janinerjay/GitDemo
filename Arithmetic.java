6973398b165f24ac7615c6110891fe668780db69
Janine Jay <janine.jay@quinnipiac.edu>
2015-10-13 21:17:34 -0400

    Update Arithmetic.java

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,5 +1,8 @@
 public class Arithmetic () {
     public static void (String[] args){
-	System.out.println("Hello World");
+	System.out.println(add(10,20));
+    }
+    public static int add(int a, int b){
+    	return a+b;
     }
 }
